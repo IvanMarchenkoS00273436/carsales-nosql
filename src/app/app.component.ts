dca253b12f2fe9a8d610565ac084822a31e0417f
IvanMarchenkoS00273436 <S00273436@atu.ie>
2025-03-27 14:27:09 +0000

    update README and enhance navigation; add project links, implement navbar with routing, and display student information in about component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CarlistComponent } from "./components/carlist/carlist.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CarlistComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
